32ff230f041c4b70b92316c8fb27715d3991c9b6
Maravillas <Maravillas@Maravillas-TOSH>
2013-12-13 01:46:09 +0100

    Comments

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 
 import java.awt.Color;
-import java.awt.RadialGradientPaint;
 import java.awt.geom.Ellipse2D;
 import java.util.Random;
 import org.jbox2d.dynamics.*;
@@ -10,8 +9,6 @@ import java.awt.Polygon;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import org.jbox2d.collision.shapes.CircleShape;
-import org.jbox2d.collision.shapes.PolygonShape;
-import org.jbox2d.collision.shapes.Shape;
 import org.jbox2d.common.Vec2;
 
 public class Main {
@@ -19,8 +16,6 @@ public class Main {
     public static void main(String[] args) {
         int WIDTH = 400;
         int HEIGHT = 300;
-        int SIZE = 1000;
-        int STRIDE = 100;
 
         //World
         float timeStep = 2.0f / 60f;
@@ -75,6 +70,7 @@ public class Main {
         Body planet = world.createBody(bdPlanet);
         planet.createFixture(fdPlanet);
 
+        //Window
         Application.run("Colors", WIDTH, HEIGHT, context -> {
 
             for (;;) {
@@ -84,22 +80,27 @@ public class Main {
                     Thread.currentThread().interrupt();
                 }
                 context.render(graphics -> {
+                    //Background
                     graphics.setColor(Color.BLACK);
                     graphics.fill(new Rectangle2D.Float(0, 0, WIDTH, HEIGHT));
                     
-                    
+                    //Motion effect
                     float x = spaceShip.getPosition().x;
                     float y = spaceShip.getPosition().y;
                     graphics.translate(-x, y);
                     
+                    //Paint starts
                     graphics.setColor(Color.WHITE);
                     for (Vec2 point : starts) {
                         graphics.fill(new Ellipse2D.Float(point.x, point.y, 4, 4));
                     }
-
+                    
+                    //Planet
                     graphics.setColor(Color.LIGHT_GRAY);
                     graphics.fill(new Ellipse2D.Float(planet.getPosition().x, planet.getPosition().y, 200, 200));
-                    graphics.setColor(Color.PINK);
+                    
+                    //Spaceship
+                    graphics.setColor(Color.orange);
                     xCoords[0] = (int)x + WIDTH / 2;
                     yCoords[0] = -(int)y + HEIGHT / 2 - (int) csSpacheShip.m_radius;
                     xCoords[1] = xCoords[0] - (side / 2);
@@ -107,19 +108,14 @@ public class Main {
                     xCoords[2] = xCoords[0] + side / 2;
                     yCoords[2] = yCoords[0] + h;
                     graphics.fill(new Polygon(xCoords, yCoords, 3));
-                    graphics.dispose();
+                    
+                    
+                    //Apply Linear Impulse
                     spaceShip.applyLinearImpulse(new Vec2(50f, 0), spaceShip.getWorldPoint(spaceShip.getWorldCenter()));
+                    
+                    //Step and dispose
+                    graphics.dispose();
                     world.step(timeStep, velocityIteration, positionIteration);
-
-                    /*for(int i = 0; i < STRIDE; i++) {
-                     float x = random.nextInt(WIDTH);
-                     float y = random.nextInt(HEIGHT);
-
-                     Color color = new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255), random.nextInt(255));
-                     RadialGradientPaint paint = new RadialGradientPaint(x, y, SIZE, new float[]{0f, 1f}, new Color[]{color, Color.WHITE});
-                     graphics.setPaint(paint);
-                     graphics.fill(new Ellipse2D.Float(x - SIZE/2, y - SIZE/2, SIZE, SIZE));
-                     }*/
                 });
             }
         });
